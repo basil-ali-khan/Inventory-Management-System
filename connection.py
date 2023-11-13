a7e034f91d12b619e2c626869c879eb1b1322cd0
Basil Ali Khan <75114167+basil-ali-khan@users.noreply.github.com>
2023-11-13 17:17:45 +0500

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,23 @@
+# Importing essential modules
+from PyQt6 import QtWidgets, uic
+from PyQt6.QtCore import QDate
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QHeaderView
+import sys
+import pyodbc
+
+server = 'DESKTOP-UMMHQQL\SQLEXPRESS01'
+database = 'Northwind'  # Name of your Northwind database
+use_windows_authentication = True  # Set to True to use Windows Authentication
+username = 'your_username'  # Specify a username if not using Windows Authentication
+password = 'your_password'  # Specify a password if not using Windows Authentication
+
+if use_windows_authentication:
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
+else:
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+# Establish a connection to the database
+connection = pyodbc.connect(connection_string)
+
+# Create a cursor to interact with the database
+cursor = connection.cursor()
