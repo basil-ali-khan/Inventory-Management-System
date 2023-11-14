6b8dd19c78fba8d7839aa95dc6d7417bd86a95be
Taha Munawar <tahamunawar16@gmail.com>
2023-11-14 22:21:19 +0500

    Latest Update
    UserAuthentication screen fully functional, with desired buttons being disabled/enabled based upon the privileges.

--- connection.py
+++ connection.py
@@ -7,18 +7,18 @@ import sys
 import pyodbc
 import VendorScreen
 
-server = 'localhost'
-database = 'Inventory_Management_System'  # Name of your Northwind database
-use_windows_authentication = False  # Set to True to use Windows Authentication
-username = 'sa'  # Specify a username if not using Windows Authentication
-password = 'Sirmehdi69'  # Specify a password if not using Windows Authentication
+# server = 'localhost'
+# database = 'Inventory_Management_System'  # Name of your Northwind database
+# use_windows_authentication = False  # Set to True to use Windows Authentication
+# username = 'sa'  # Specify a username if not using Windows Authentication
+# password = 'Sirmehdi69'  # Specify a password if not using Windows Authentication
 
-if use_windows_authentication:
-    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
-else:
-    connection_string = (
-        'DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost;DATABASE=Inventory_Management_System;UID=sa;PWD=Sirmehdi69;TrustServerCertificate=yes;Connection Timeout=30;'
-    )
+# if use_windows_authentication:
+#     connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
+# else:
+connection_string = (
+    'DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost;DATABASE=Inventory_Management_System;UID=sa;PWD=Sirmehdi69;TrustServerCertificate=yes;Connection Timeout=30;'
+)
 
 # Establish a connection to the database
 connection = pyodbc.connect(connection_string)
@@ -97,9 +97,20 @@ class UI(QtWidgets.QMainWindow):
         
         # Load the .ui file
         uic.loadUi('Screens/UserAuthentication.ui', self)
+        self.password.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
 
+        self.username.setPlaceholderText("Username")
+        self.password.setPlaceholderText("Password")
         ###hardcoded for now, will later be enabled only if user has admin priveleges
-        self.vendorsButton.setEnabled(True)
+
+        self.vendorsButton.setEnabled(False)
+        self.customerButton.setEnabled(False)
+        self.productsButton.setEnabled(False)
+        self.salesButton.setEnabled(False)
+        self.materialButton.setEnabled(False)
+        self.purchaseButton.setEnabled(False)
+
+        self.loginButton.clicked.connect(self.CheckPrivilege)
         self.vendorsButton.clicked.connect(self.OpenVendorScreen)
 
 
@@ -107,6 +118,43 @@ class UI(QtWidgets.QMainWindow):
         self.vendor = VendorScreen()
         self.vendor.show()
         
+    def CheckPrivilege(self):
+        username = self.username.text()
+        password = self.password.text()
+        sql_query = "select privilege from [User] where userName = ? and password = ?"
+        cursor.execute(sql_query, (username, password))
+        result = cursor.fetchone()
+        if result is not None:
+            result = result[0]
+            if result == 'Admin':
+                self.vendorsButton.setEnabled(True)
+                self.customerButton.setEnabled(True)
+                self.productsButton.setEnabled(True)
+                self.salesButton.setEnabled(True)
+                self.materialButton.setEnabled(True)
+                self.purchaseButton.setEnabled(True)
+            elif result == 'User':
+                self.vendorsButton.setEnabled(False)
+                self.customerButton.setEnabled(False)
+                self.productsButton.setEnabled(False)
+                self.salesButton.setEnabled(True)
+                self.materialButton.setEnabled(False)
+                self.purchaseButton.setEnabled(False)
+        else:
+            self.addMsg = AddVendorMessageBox("Incorrect Username and/or Password", "Error")
+            self.addMsg.show()
+            self.vendorsButton.setEnabled(False)
+            self.customerButton.setEnabled(False)
+            self.productsButton.setEnabled(False)
+            self.salesButton.setEnabled(False)
+            self.materialButton.setEnabled(False)
+            self.purchaseButton.setEnabled(False)
+
+        self.username.clear()
+        self.password.clear()
+
+
+
 
 app = QApplication(sys.argv)
 loginScreen = UI()
