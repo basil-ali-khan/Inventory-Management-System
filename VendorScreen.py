f7a656594a5ce2178d589d55be6be69048ff6890
basil-ali-khan <khanbasil2002@gmail.com>
2023-11-14 10:24:52 +0500

    Populated Vendors Table

---
+++ VendorScreen.py
@@ -0,0 +1,22 @@
+# Importing essential modules
+from PyQt6 import QtWidgets, uic
+from PyQt6.QtCore import QDate
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QHeaderView
+import sys
+import pyodbc
+
+server = 'DESKTOP-UMMHQQL\SQLEXPRESS01'
+database = 'Northwind'  # Name of your Northwind database
+use_windows_authentication = True  # Set to True to use Windows Authentication
+username = 'your_username'  # Specify a username if not using Windows Authentication
+password = 'your_password'  # Specify a password if not using Windows Authentication
+
+
+
+class VendorScreen(QtWidgets.QMainWindow):   
+    def __init__(self):
+        # Call the inherited classes __init__ method
+        super(VendorScreen, self).__init__() 
+        
+        # Load the .ui file
+        uic.loadUi('Screens/Vendors.ui', self)
--- connection.py
+++ connection.py
@@ -4,9 +4,10 @@ from PyQt6.QtCore import QDate
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QHeaderView
 import sys
 import pyodbc
+import VendorScreen
 
 server = 'DESKTOP-UMMHQQL\SQLEXPRESS01'
-database = 'Northwind'  # Name of your Northwind database
+database = 'Inventory_Management_System'  # Name of your Northwind database
 use_windows_authentication = True  # Set to True to use Windows Authentication
 username = 'your_username'  # Specify a username if not using Windows Authentication
 password = 'your_password'  # Specify a password if not using Windows Authentication
@@ -22,8 +23,35 @@ connection = pyodbc.connect(connection_string)
 # Create a cursor to interact with the database
 cursor = connection.cursor()
 
-class UI(QtWidgets.QMainWindow):   
+class VendorScreen(QtWidgets.QMainWindow):   
+    def __init__(self):
+        # Call the inherited classes __init__ method
+        super(VendorScreen, self).__init__() 
+        
+        # Load the .ui file
+        uic.loadUi('Screens/Vendors.ui', self)
 
+        self.PopulateVendorTable()        
+
+
+    def PopulateVendorTable(self):
+        cursor.execute("SELECT * FROM Vendor")
+        self.vendorTable.setRowCount(0)
+
+        for row_index, row_data in enumerate(cursor.fetchall()):
+            self.vendorTable.insertRow(row_index)
+            for col_index, cell_data in enumerate(row_data):
+                item = QTableWidgetItem(str(cell_data))
+                self.vendorTable.setItem(row_index, col_index, item)
+
+        # cursor.execute("Select count(vendorID) from Vendor")
+        # row_count = 1
+        # for i in cursor.fetchall():
+        #     row_count += 1
+        # # row_count = len(cursor.fetchall())
+        # self.vendorTable.setRowCount(row_count)
+
+class UI(QtWidgets.QMainWindow):   
     def __init__(self):
         # Call the inherited classes __init__ method
         super(UI, self).__init__() 
@@ -31,9 +59,15 @@ class UI(QtWidgets.QMainWindow):
         # Load the .ui file
         uic.loadUi('Screens/UserAuthentication.ui', self)
 
+        ###hardcoded for now, will later be enabled only if user has admin priveleges
+        self.vendorsButton.setEnabled(True)
+        self.vendorsButton.clicked.connect(self.OpenVendorScreen)
 
 
-
+    def OpenVendorScreen(self):
+        self.vendor = VendorScreen()
+        self.vendor.show()
+        
 
 app = QApplication(sys.argv)
 loginScreen = UI()
