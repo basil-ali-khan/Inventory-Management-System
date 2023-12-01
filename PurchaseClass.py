5a72f4124539ebed91b4e0ffb27c60dc01192df8
basil-ali-khan <khanbasil2002@gmail.com>
2023-12-01 21:15:08 +0500

    Pushing Purchase to main

--- PurchaseClass.py
+++ PurchaseClass.py
@@ -9,19 +9,40 @@ from AddPurchaseClass import AddPurchaseScreen
 from ViewPurchaseClass import ViewPurchaseScreen
 
 # server = 'DESKTOP-UMMHQQL\SQLEXPRESS01'
-server = 'LAPTOP-MNMD5RBU'
-database = 'Inventory_Management_System_Script'  # Name of your Northwind database
+# server = 'LAPTOP-MNMD5RBU'
+# database = 'Inventory_Management_System_Script'  # Name of your Northwind database
+# use_windows_authentication = True  # Set to True to use Windows Authentication
+# username = 'your_username'  # Specify a username if not using Windows Authentication
+# password = 'your_password'  # Specify a password if not using Windows Authentication
+
+# if use_windows_authentication:
+#     connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
+# else:
+#     connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+# connection = pyodbc.connect(connection_string)
+# cursor = connection.cursor()
+
+server = 'DESKTOP-UMMHQQL\SQLEXPRESS01'
+database = 'Inventory_Management_System'  # Name of your Northwind database
 use_windows_authentication = True  # Set to True to use Windows Authentication
-username = 'your_username'  # Specify a username if not using Windows Authentication
-password = 'your_password'  # Specify a password if not using Windows Authentication
+username = 'sa'  # Specify a username if not using Windows Authentication
+password = 'Sirmehdi69'  # Specify a password if not using Windows Authentication
 
 if use_windows_authentication:
     connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
 else:
-    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    connection_string = (
+        'DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost;DATABASE=Inventory_Management_System;UID=sa;PWD=Sirmehdi69;TrustServerCertificate=yes;Connection Timeout=30;'
+    )
 
+# Establish a connection to the database
 connection = pyodbc.connect(connection_string)
+
+# Create a cursor to interact with the database
 cursor = connection.cursor()
+
+
 
 class PurchaseScreen(QtWidgets.QMainWindow):   
 
