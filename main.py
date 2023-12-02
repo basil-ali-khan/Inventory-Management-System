24c5e90ccb4e23d4b7260ff98ebb4c84b73e8d9c
Taha Munawar <tahamunawar16@gmail.com>
2023-12-02 15:55:49 +0500

    Added Connection String file

--- main.py
+++ main.py
@@ -9,24 +9,7 @@ import CustomerClass
 import MaterialClass
 import ProductsClass
 
-server = 'DESKTOP-UMMHQQL\SQLEXPRESS01'
-database = 'Inventory_Management_System'  # Name of your Northwind database
-use_windows_authentication = False  # Set to True to use Windows Authentication
-username = 'sa'  # Specify a username if not using Windows Authentication
-password = 'Sirmehdi69'  # Specify a password if not using Windows Authentication
-
-if use_windows_authentication:
-    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
-else:
-    connection_string = (
-        'DRIVER={ODBC Driver 18 for SQL Server};SERVER=localhost;DATABASE=Inventory_Management_System;UID=sa;PWD=Sirmehdi69;TrustServerCertificate=yes;Connection Timeout=30;'
-    )
-
-# Establish a connection to the database
-connection = pyodbc.connect(connection_string)
-
-# Create a cursor to interact with the database
-cursor = connection.cursor()
+from ConnectionString import connection, cursor
 
 class UI(QtWidgets.QMainWindow):
     def __init__(self):
